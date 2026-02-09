5d1f1f9c7d271dbff279038279b7fee17eb3acdb
Karl Lowe <247839965+karllowe@users.noreply.github.com>
2026-02-09 12:30:44 +0000

    added shuffle animation

--- src/ImageGrid.jsx
+++ src/ImageGrid.jsx
@@ -1,6 +1,7 @@
 // import { useState } from "react";
 import "/src/styles/imageGrid.css";
 import { PokemonCard } from "./PokemonCard";
+import {motion} from "framer-motion";
 
 function ImageGrid({pokemonArray, score, setScore, resetGame, selectedPokemonArray, setSelectedPokemonArray, shufflePokemonArray}) {
 
@@ -8,7 +9,9 @@ function ImageGrid({pokemonArray, score, setScore, resetGame, selectedPokemonArr
         <div className="imageGrid">
             {
                 pokemonArray.map((item) => (
+                    <motion.div key={item.id} layout transition={{duration: 0.5, ease: "easeInOut"}}>
                     <PokemonCard key={item.id} pokemon={item} score={score} setScore={setScore} resetGame={resetGame} selectedPokemonArray={selectedPokemonArray} setSelectedPokemonArray={setSelectedPokemonArray} shufflePokemonArray={shufflePokemonArray}/>
+                    </motion.div>
                 ))
             }
         </div>
